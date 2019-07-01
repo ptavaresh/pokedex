de338302dbb909ff95d35b54c458e862bccb0172
Pedro Tavares <ptavaresh@gmail.com>
2019-07-01 18:10:10 -0500

    Starting project

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import PokeSearch from './PokeSearch'
+
+const App = () => {
+    return (
+
+    <div className="ui container" style={{marginTop: '10px'}}>
+        <PokeSearch />    
+    </div>
+    )
+};
+
+export default App;
---
+++ src/components/PokeSearch.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+
+class PokeSearch extends React.Component {
+
+    state = {
+        name: ''
+    };
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.name);
+    }
+
+    render() {
+        return (
+            <div className="ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Pokemon search</label>
+                        <input 
+                            type='text' 
+                            value={this.state.name}
+                            onChange={(e) => this.setState({term: e.target.value})} 
+                        />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default PokeSearch;
