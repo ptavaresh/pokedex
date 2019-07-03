4cb54dbecf37647a67c5d1233723abb6c7d398e2
Pedro Tavares <ptavaresh@gmail.com>
2019-07-03 16:51:28 -0500

    Adding the picture to the selected pokemon

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,26 @@
 import React from 'react';
 
 import PokeSearch from './PokeSearch';
+import PokemonInfo from './PokemonInfo';
 
 import pokeapi from '../api/pokeapi';
 
 class App extends React.Component {
-    state = { pokemons: [] };
+    state = { pokemons: [], picture: '' };
   
     onSearchSubmit = async name => {
-      const response = await pokeapi.get('ditto'//, {
-        //params: { query: name }
-      //}
-      //
-      );
-    console.log(response.data.name)
+      const response = await pokeapi.get(name);
+
     console.log(response)
-    console.log(response.data.sprites.back_default)
-      this.setState({ pokemons: response.data });
+
+      this.setState({ pokemons: response.data, picture: response.data.sprites.front_default });
     };
   
     render() {
       return (
         <div className="ui container" style={{ marginTop: '10px' }}>
           <PokeSearch onSubmit={this.onSearchSubmit} />
-          Name: {this.state.pokemons.name}
-          ID: {this.state.pokemons.id}
-          <img src={this.state.pokemons.sprites}/>
-
+          <PokemonInfo pokemons={this.state.pokemons} picture={this.state.picture} />
         </div>
       );
     }
---
+++ src/components/PokemonInfo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class PokemonInfo extends React.Component {
+    render () {
+        
+        return (
+            <div className="ui center aligned segment" style={{ marginTop: '10px' }}>
+                <h1>{this.props.pokemons.name}</h1>
+                <p>{this.props.pokemons.id}</p>
+                <img src={this.props.picture} alt={this.props.pokemons.name}/>
+            </div>
+        )
+    }
+}
+
+export default PokemonInfo;
